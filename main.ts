66dd2ec30083673b7ae5238ca91c991c4e8b3a26
Andrew Brown <seatrout@gmail.com>
2021-12-04 20:27:35 +0000

    should work

--- main.ts
+++ main.ts
@@ -37,15 +37,7 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		this.addCommand({
+this.addCommand({
 			id: 'sidenote-open',
 			name: 'make an open side note',
 			editorCallback:(editor:Editor,view:MarkdownView)=> {
@@ -66,24 +58,6 @@ export default class MyPlugin extends Plugin {
 			}
 		})
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
